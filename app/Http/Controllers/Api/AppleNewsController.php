a366ba9c5e14062e447e671581efaf0c3d3a83b4
StanleyCocos <stanleycocoa@gmail.com>
2021-10-12 17:53:50 +0800

    测试

--- app/Http/Controllers/Api/AppleNewsController.php
+++ app/Http/Controllers/Api/AppleNewsController.php
@@ -14,9 +14,6 @@ class AppleNewsController extends Controller
         
         if(!$service->exist($request->all())){
             $appleNews = $service->store($request->all());
-            $http = new GuzzleHttp\Client;
-            $response = $http->request('GET', 'http://eagle.addcn.com/jalert/sendnotify.cgi?receiver=10456,10400&title=apple新规&msg='.$appleNews->title);
-            return $response->getStatusCode();
             return $transformers->transform($appleNews);
         }        
         return ["code"=>200, "msg"=> "重复"];
